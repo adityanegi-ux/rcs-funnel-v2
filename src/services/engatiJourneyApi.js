c5b37ab2516a4a775a63aee5210c3af05282b623
aditya.negi <aditya.negi@engati.com>
2026-03-06 12:15:29 +0530

    field name updates

--- src/services/engatiJourneyApi.js
+++ src/services/engatiJourneyApi.js
@@ -98,9 +98,10 @@ export async function capturePage2Journey({ fullName, email, phoneNumber }) {
   const sessionId = getOrCreateLeadSessionId();
   const payload = {
     ...buildCommonPayload('page_2', sessionId),
-    user_name: fullName || '',
-    email: email || '',
-    p2_mobile_e164_no_plus: normalizeIndianMobileNo(phoneNumber),
+    'user.user_name': fullName || '',
+    'user.email': email || '',
+    'user.phone_no': normalizeIndianMobileNo(phoneNumber),
+    'user.channel': 'web',
     p2_timestamp_utc: getUtcIsoTimestamp(),
   };
 
