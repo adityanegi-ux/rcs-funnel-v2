1a1d2402db7949445a898e5697205c138c17fbbe
aditya.negi <aditya.negi@engati.com>
2026-03-09 20:52:02 +0530

    negi - updated endpoint for 2nd page

--- server/engatiFlowEndpoints.js
+++ server/engatiFlowEndpoints.js
@@ -2,7 +2,7 @@ export const ENGATI_FLOW_ENDPOINTS = {
   journeyStart:
     'https://api.engati.ai/bot-api/v2.0/customer/28459/bot/8419bd588bec4048/flow/EBAE1F6090D54CC3B7689F02C04A004F',
   identityCapture:
-    'https://api.engati.ai/bot-api/v2.0/customer/28459/bot/8419bd588bec4048/flow/EBAE1F6090D54CC3B7689F02C04A004F',
+    'https://api.engati.ai/bot-api/v2.0/customer/28459/bot/8419bd588bec4048/flow/B870866FE21F4CC08F60BAD184D26609',
   rcsProfileSubmit:
     'https://api.engati.ai/bot-api/v2.0/customer/28459/bot/8419bd588bec4048/flow/B870866FE21F4CC08F60BAD184D26609',
 };
